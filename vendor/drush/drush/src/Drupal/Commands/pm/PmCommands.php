4b5f675aedb674b6d680e90d636a2549012f53c4
Randy Fay <randy@randyfay.com>
2022-06-18 11:36:19 -0600

    Update to drupal 9.4.0

--- vendor/drush/drush/src/Drupal/Commands/pm/PmCommands.php
+++ vendor/drush/drush/src/Drupal/Commands/pm/PmCommands.php
@@ -65,12 +65,12 @@ class PmCommands extends DrushCommands
     /**
      * Enable one or more modules.
      *
-     * @command pm:enable
+     * @command pm:install
      * @param $modules A comma delimited list of modules.
-     * @aliases en,pm-enable
+     * @aliases in, install, pm-install, en, pm-enable, pm:enable
      * @bootstrap root
      */
-    public function enable(array $modules): void
+    public function install(array $modules): void
     {
         $modules = StringUtils::csvToArray($modules);
         $todo = $this->addInstallDependencies($modules);
@@ -97,7 +97,7 @@ class PmCommands extends DrushCommands
     /**
      * Run requirements checks on the module installation.
      *
-     * @hook validate pm:enable
+     * @hook validate pm:install
      *
      * @throws UserAbortException
      * @throws MissingDependencyException
@@ -116,7 +116,15 @@ class PmCommands extends DrushCommands
         require_once DRUSH_DRUPAL_CORE . '/includes/install.inc';
         $error = false;
         foreach ($modules as $module) {
-            module_load_install($module);
+            // Note: we can't just call the API ($moduleHandler->loadInclude($module, 'install')),
+            // because the API ignores modules that haven't been installed yet. We have
+            // to do it the same way the `function drupal_check_module($module)` does.
+            $module_list = \Drupal::service('extension.list.module');
+            $file = DRUPAL_ROOT . '/' . $module_list->getPath($module) . "/$module.install";
+            if (is_file($file)) {
+                require_once $file;
+            }
+            // Once we've loaded the module, we can invoke its requirements hook.
             $requirements = $this->getModuleHandler()->invoke($module, 'requirements', ['install']);
             if (is_array($requirements) && drupal_requirements_severity($requirements) == REQUIREMENT_ERROR) {
                 $error = true;
